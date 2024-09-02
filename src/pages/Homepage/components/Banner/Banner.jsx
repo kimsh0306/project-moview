a3b0c8e589305c19aeddecaef1cb8bce49afe551
kimsh0306 <b666790@gmail.com>
2024-09-02 11:20:00 +0900

    movie info btn

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -3,13 +3,27 @@ import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
 import Alert from "react-bootstrap/Alert";
 import "./Banner.style.css";
 import { InfoOutlined } from "@mui/icons-material";
-import IconButton from "@mui/material/IconButton";
-import Button from "@mui/material/Button";
-import { red } from "@mui/material/colors";
+import { useNavigate } from "react-router-dom";
 
-const primary = red[500];
+import IconButton from "@mui/material/IconButton";
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+
 
 const Banner = () => {
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: "white",
+    backgroundColor: "none",
+    border:"1px solid #999",
+    '&:hover': {
+      backgroundColor: "white",
+      backgroundColor: "none",
+      border:"1px solid white",
+    },
+  }));
+
+  const navigate = useNavigate();
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
   console.log("data: ", data);
   if (isLoading) {
@@ -19,32 +33,35 @@ const Banner = () => {
     <Alert variant="danger">{error.message}</Alert>;
   }
   return (
-    <div className="banner-box">
-      <div
-        className="banner"
-        style={{
-          backgroundImage:
-            "url(" +
-            `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}` +
-            ")",
-        }}
-      ></div>
+    <div className="banner">
+      <div className="banner-img-box">
+        <div
+          className="banner-img"
+          style={{
+            backgroundImage:
+              "url(" +
+              `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}` +
+              ")",
+          }}
+        ></div>
+      </div>
       <div className="text-white banner-text-area">
         <h1>{data?.results[0].title}</h1>
         <p>{data?.results[0].overview}</p>
 
         {/* <InfoOutlined className="movie-info" /> */}
-        {/* <Button
-          component="label"
-          role={undefined}
+        <ColorButton
+          onClick={() => {navigate("/movies/:id")}}
+          sx={{
+            marginTop: "14px",
+          }}
           variant="outlined"
           size="small"
-          tabIndex={-1}
           color="primary"
           startIcon={<InfoOutlined />}
         >
           movie info
-        </Button> */}
+        </ColorButton>
       </div>
     </div>
   );
