63d6467a3455011b0d40bc3f0aea434a745dbc05
kimsh0306 <b666790@gmail.com>
2024-09-04 19:28:18 +0900

    Banner img moving

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -6,20 +6,18 @@ import { InfoOutlined } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 
 import IconButton from "@mui/material/IconButton";
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
 
 const Banner = () => {
-
   const ColorButton = styled(Button)(({ theme }) => ({
     color: "white",
     backgroundColor: "none",
-    border:"1px solid #999",
-    '&:hover': {
+    border: "1px solid #999",
+    "&:hover": {
       backgroundColor: "white",
       backgroundColor: "none",
-      border:"1px solid white",
+      border: "1px solid white",
     },
   }));
 
@@ -32,10 +30,12 @@ const Banner = () => {
   if (isError) {
     <Alert variant="danger">{error.message}</Alert>;
   }
+
+  const imgUrl = `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}`;
   return (
     <div className="banner">
       <div className="banner-img-box">
-        <div
+        {/* <div
           className="banner-img"
           style={{
             backgroundImage:
@@ -43,7 +43,12 @@ const Banner = () => {
               `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}` +
               ")",
           }}
-        ></div>
+        ></div> */}
+        <div className="banner-img">
+          <div className="moving-img">
+            <img className="img" src={imgUrl} />
+          </div>
+        </div>
       </div>
       <div className="text-white banner-text-area">
         <div className="top1-badge">
@@ -55,7 +60,9 @@ const Banner = () => {
 
         {/* <InfoOutlined className="movie-info" /> */}
         <ColorButton
-          onClick={() => {navigate("/movies/:id")}}
+          onClick={() => {
+            navigate("/movies/:id");
+          }}
           sx={{
             marginTop: "14px",
           }}
