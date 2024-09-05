4abb77f8d487d7f04d9d254c02c1d8fe30f7ee32
kimsh0306 <b666790@gmail.com>
2024-09-05 11:52:16 +0900

    select css

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -7,24 +7,70 @@ import ReactPaginate from "react-paginate";
 import RecommendMovie from "./components/RecommendMovie/RecommendMovie";
 import "./MoviePage.style.css";
 
-import { deepOrange, amber, grey } from "@mui/material/colors";
+import { deepOrange, green,amber, grey } from "@mui/material/colors";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
+  components: {
+    MuiSelect: {
+      styleOverrides: {
+        root: {
+          color: grey[100],
+          ".MuiOutlinedInput-notchedOutline": {
+            borderColor: grey[500],
+          },
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: grey[500],
+          },
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: grey[500],
+          },
+          ".MuiSvgIcon-root ": {
+            fill: `${grey[500]} !important`,
+          },
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides:{
+        root:{
+          backgroundColor: grey[100],
+        }
+      }
+    },
+    // MuiButtonBase: {
+    //   styleOverrides:{
+    //     root:{
+    //       color:  deepOrange[500],
+    //       backgroundColor: grey[300],
+    //     }
+    //   }
+    // },
+    // MuiPaper: {
+    //   styleOverrides:{
+    //     root:{
+    //       backgroundColor: grey[300],
+    //     }
+    //   }
+    // },
+  },
   palette: {
-    primary: grey,
-    secondary: grey,
+    primary: {
+      main: grey[200],
+      light: grey[500],
+      dark: grey[700],
+    },
   },
 });
 
 const MoviePage = () => {
   const [query, setQuery] = useSearchParams();
   const [page, setPage] = useState(1);
-  const [age, setAge] = React.useState("");
+  const [sort, setSort] = React.useState("");
 
   const keyword = query.get("q");
   const { data, isLoading, isError, error } = useSearchMovieQuery(
@@ -32,14 +78,25 @@ const MoviePage = () => {
     page
   );
 
+  console.log("selecting data:", data);
+
   const handleChange = (event) => {
-    setAge(event.target.value);
+    // console.log("event: ", event.target.value)
+    const sortType = event.target.value;
+    setSort(sortType);
+
+    if (sortType === "popularity") {
+      
+    }
   };
 
   const handlePageClick = ({ selected }) => {
-    console.log("selected:", selected);
     setPage(selected + 1);
   };
+
+  // useEffect(()=> {
+  //   console.log("sort: ", sort);
+  // },[sort]);
 
   if (data?.results.length === 0) {
     return <RecommendMovie />;
@@ -57,24 +114,35 @@ const MoviePage = () => {
       <Container className="movie-container">
         <Row>
           <Col lg={4} xs={12}>
-          
             <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <InputLabel 
+              sx={{ color: grey[500] }}
+              id="demo-simple-select-label">sort</InputLabel>
               <Select
+                labelstyle={{ color: "#ff0000" }}
                 selected
-                className="select"
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                value={age}
-                label="Age"
+                value={sort}
+                label="sort"
                 onChange={handleChange}
-                sx={{
-                  color:"white",
-                }}
+                // sx={{
+                //   color: "white",
+                //   ".MuiOutlinedInput-notchedOutline": {
+                //     borderColor: "white",
+                //   },
+                //   "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                //     borderColor: "white",
+                //   },
+                //   "&:hover .MuiOutlinedInput-notchedOutline": {
+                //     borderColor: "white",
+                //   },
+                //   ".MuiSvgIcon-root ": {
+                //     fill: "white !important",
+                //   },
+                // }}
               >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value="popularity">popularity</MenuItem>
               </Select>
             </FormControl>
           </Col>
