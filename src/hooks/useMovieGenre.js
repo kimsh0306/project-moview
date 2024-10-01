dfd68da1c22a3476b61260075220db83dd8cc098
kimsh0306 <b666790@gmail.com>
2024-10-01 11:12:07 +0900

    Fix: 장르 받아오는 react-query 수정

--- src/hooks/useMovieGenre.js
+++ src/hooks/useMovieGenre.js
@@ -10,6 +10,8 @@ export const useMovieGenreQuery = () => {
     queryKey: ['movie-genre'],
     queryFn: fetchMovieGenre,
     select:(result) => result.data.genres,
-    staleTime: 300000, //5분
+    staleTime: 1000 * 60 * 60 * 24, // 24시간
+    refetchOnWindowFocus: false, // 브라우저 창 또는 탭이 다시 포커스를 받을 때 리패치 방지
+    refetchOnMount: false, // 마운트 시 리패치 방지
   });
 };
