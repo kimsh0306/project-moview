71fc45d40c30a361105c1737be4d52b7c0d6848b
kimsh0306 <b666790@gmail.com>
2024-10-07 17:49:33 +0900

    Fix: 테스트 push

--- frontend/src/layout/AppLayout.js
+++ frontend/src/layout/AppLayout.js
@@ -65,7 +65,7 @@ const AppLayout = () => {
       <Navbar bg="black" data-bs-theme="dark" expand="lg">
         <Container fluid>
           <Navbar.Brand onClick={() => { navigate('/') }}>
-            moView
+            Moview
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
