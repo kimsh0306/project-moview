ef1ded9cc7f797ff4d554409e208ea00d0958de3
kimsh0306 <b666790@gmail.com>
2024-10-15 23:16:53 +0900

    Test: 찜한 영화 표시

--- frontend/src/pages/MyListPage/MyListPage.jsx
+++ frontend/src/pages/MyListPage/MyListPage.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
-import CustomDropdown from "../Movies/components/CustomDropdown/CustomDropdown";
+import { useDispatch, useSelector } from "react-redux";
+import { Container, Row, Col } from "react-bootstrap";
 import "./MyListPage.style.css";
+import MovieCard from "../../common/MovieCard/MovieCard";
 
 const MyListPage = () => {
-  const [sort, setSort] = React.useState("popularity");
+  const myMovies = useSelector((state) => state.myMovies);
 
   return (
     <div className="my-list">
       <Container fluid>
-        <Row className="movie-list__header">
-          <div className="page-name">
+        <Row className="header">
+          <div className="title">
             <h2>내가 찜한 영화</h2>
-            <div className="dropdown">
-              <CustomDropdown sort={sort} setSort={setSort} />
-            </div>
           </div>
+        </Row>
+        <Row className="contents">
+          {myMovies.map((movie, idx) => (
+            <Col key={idx} xl={2} lg={3} md={4} sm={6} xs={6}>
+              <MovieCard movie={movie} />
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
