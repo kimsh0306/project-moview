9c900998172bfa2efe540210d69cef56781413b7
kimsh0306 <b666790@gmail.com>
2024-10-15 20:28:14 +0900

    Fix: 재사용성을 위해 코드 수정

--- frontend/src/common/FavoriteMark/FavoriteMark.jsx
+++ frontend/src/common/FavoriteMark/FavoriteMark.jsx
@@ -1,29 +1,119 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import { BsBookmarkPlus, BsBookmarkDashFill } from "react-icons/bs";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Tooltip from "react-bootstrap/Tooltip";
+import CheckLoginModal from "../CheckLoginModal/CheckLoginModal";
+import LoadingModal from "../LoadingModal/LoadingModal";
 
-const FavoriteMark = ({ isFavorite, handleFavoriteItem, fontSize = "1.7rem" }) => {
-  return isFavorite ? (
-    <OverlayTrigger overlay={<Tooltip>찜목록에서 제거</Tooltip>}>
-      <div>
-        <BsBookmarkDashFill
-          className="favorite-selected"
-          onClick={handleFavoriteItem}
-          style={{ fontSize: fontSize }}
-        />
-      </div>
-    </OverlayTrigger>
-  ) : (
-    <OverlayTrigger overlay={<Tooltip>찜목록에 추가</Tooltip>}>
-      <div>
-        <BsBookmarkPlus
-          className="favorite-unselected"
-          onClick={handleFavoriteItem}
-          style={{ fontSize: fontSize }}
-        />
-      </div>
-    </OverlayTrigger>
+const FavoriteMark = ({ movie, fontSize = "1.7rem" }) => {
+  const [isFavorite, setIsFavorite] = useState();
+  const [showCheckLoginModal, setShowCheckLoginModal] = useState(false);
+  const [showLoading, setShowLoading] = useState(false);
+  const myMovies = useSelector((state) => state.myMovies);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!movie) return;
+
+    if (myMovies.some((myMovie) => myMovie.id === movie.id)) {
+      setIsFavorite(true);
+    } else {
+      setIsFavorite(false);
+    }
+  }, [movie, myMovies]);
+
+  const handleFavoriteMark = (e) => {
+    const userId = localStorage.getItem("userId");
+    const userNum = localStorage.getItem("userNum");
+
+    e.stopPropagation();
+    if (!userId || !userNum) {
+      console.log("로그인이 필요한 서비스입니다.");
+      setShowCheckLoginModal(true);
+      return;
+    }
+    const payload = {
+      id: movie.id,
+      title: movie.title,
+      poster_path: movie.poster_path,
+      adult: movie.adult,
+      vote_average: movie.vote_average,
+      genre_ids: movie.genre_ids,
+    };
+    // db에 추가/삭제
+    if (isFavorite) {
+      setShowLoading(true);
+      axios
+        .delete(
+          `https://project-moview-api.vercel.app/users/${userNum}/my_movies/${movie.id}`
+        )
+        .then((res) => {
+          console.log("my_movies 삭제 성공:", res.data);
+          dispatch({ type: "DELETE_MY_MOVIE", payload: { myMovie: payload } });
+          setIsFavorite(false);
+        })
+        .catch((error) => {
+          const { message } = error.response.data;
+          console.error(error);
+          console.log("my_movies 삭제 실패:", message);
+        })
+        .finally(() => {
+          setShowLoading(false);
+        });
+    } else {
+      setShowLoading(true);
+      axios
+        .post(
+          `https://project-moview-api.vercel.app/users/${userNum}/my_movies`,
+          payload
+        )
+        .then((res) => {
+          console.log("my_movies 추가 성공:", res.data);
+          dispatch({ type: "PUT_MY_MOVIE", payload: { myMovie: payload } });
+          setIsFavorite(true);
+        })
+        .catch((error) => {
+          const { message } = error.response.data;
+          console.error(error);
+          console.log("my_movies 추가 실패:", message);
+        })
+        .finally(() => {
+          setShowLoading(false);
+        });
+    }
+  };
+
+  return (
+    <>
+      {isFavorite ? (
+        <OverlayTrigger overlay={<Tooltip>찜목록에서 제거</Tooltip>}>
+          <div>
+            <BsBookmarkDashFill
+              className="favorite-selected"
+              onClick={handleFavoriteMark}
+              style={{ fontSize: fontSize }}
+            />
+          </div>
+        </OverlayTrigger>
+      ) : (
+        <OverlayTrigger overlay={<Tooltip>찜목록에 추가</Tooltip>}>
+          <div>
+            <BsBookmarkPlus
+              className="favorite-unselected"
+              onClick={handleFavoriteMark}
+              style={{ fontSize: fontSize }}
+            />
+          </div>
+        </OverlayTrigger>
+      )}
+      <CheckLoginModal
+        show={showCheckLoginModal}
+        setShow={setShowCheckLoginModal}
+      />
+      <LoadingModal show={showLoading} />
+    </>
   );
 };
 
--- frontend/src/common/MovieCard/MovieCard.jsx
+++ frontend/src/common/MovieCard/MovieCard.jsx
@@ -1,100 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
 import { useMovieGenreQuery } from "../../hooks/useMovieGenre";
 import { Badge } from "react-bootstrap";
 import CircularProgressComp from "./components/CircularProgressComp/CircularProgressComp";
 import AdultBadge from "./components/AdultBadge/AdultBadge";
 import FavoriteMark from "../FavoriteMark/FavoriteMark";
-import CheckLoginModal from "../CheckLoginModal/CheckLoginModal";
-import LoadingModal from "../LoadingModal/LoadingModal";
 import "./MovieCard.style.css";
 
 const MovieCard = ({ movie }) => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const myMovies = useSelector((state) => state.myMovies);
-  const [userData, setUserData] = useState();
-  const [isFavorite, setIsFavorite] = useState();
-  const [showCheckLoginModal, setShowCheckLoginModal] = useState(false);
-  const [showLoading, setShowLoading] = useState(false);
-
   const { data: genreData } = useMovieGenreQuery();
-
-  useEffect(() => {
-    if (!movie) return;
-    const userId = localStorage.getItem("userId");
-    const userNum = localStorage.getItem("userNum");
-
-    if (userId && userNum) {
-      setUserData({ userId, userNum });
-    }
-
-    if (myMovies.some((myMovie) => myMovie.id === movie.id)) {
-      setIsFavorite(true);
-    } else {
-      setIsFavorite(false);
-    }
-  }, [movie, myMovies]);
-
-  const handleFavoriteItem = (e) => {
-    e.stopPropagation();
-    if (!userData) {
-      console.log("로그인이 필요한 서비스입니다.");
-      setShowCheckLoginModal(true);
-      return;
-    }
-    const payload = {
-      id: movie.id,
-      title: movie.title,
-      poster_path: movie.poster_path,
-      adult: movie.adult,
-      vote_average: movie.vote_average,
-      genre_ids: movie.genre_ids,
-    };
-    // db에 추가/삭제
-    if (isFavorite) {
-      setShowLoading(true);
-      axios
-        .delete(
-          `https://project-moview-api.vercel.app/users/${userData.userNum}/my_movies/${movie.id}`
-        )
-        .then((res) => {
-          console.log("my_movies 삭제 성공:", res.data);
-          dispatch({ type: "DELETE_MY_MOVIE", payload: { myMovie: payload } });
-          setIsFavorite(false);
-        })
-        .catch((error) => {
-          const { message } = error.response.data;
-          console.error(error);
-          console.log("my_movies 삭제 실패:", message);
-        })
-        .finally(() => {
-          setShowLoading(false);
-        });
-    } else {
-      setShowLoading(true);
-      axios
-        .post(
-          `https://project-moview-api.vercel.app/users/${userData.userNum}/my_movies`,
-          payload
-        )
-        .then((res) => {
-          console.log("my_movies 추가 성공:", res.data);
-          dispatch({ type: "PUT_MY_MOVIE", payload: { myMovie: payload } });
-          setIsFavorite(true);
-        })
-        .catch((error) => {
-          const { message } = error.response.data;
-          console.error(error);
-          console.log("my_movies 추가 실패:", message);
-        })
-        .finally(() => {
-          setShowLoading(false);
-        });
-    }
-  };
+  const navigate = useNavigate();
 
   const showGenre = (genreIdList) => {
     if (!genreData) return [];
@@ -136,10 +51,7 @@ const MovieCard = ({ movie }) => {
               bottom: 10,
             }}
           >
-            <FavoriteMark
-              isFavorite={isFavorite}
-              handleFavoriteItem={handleFavoriteItem}
-            />
+            <FavoriteMark movie={movie} />
           </div>
           <div className="vote-avg">
             <CircularProgressComp
@@ -149,11 +61,6 @@ const MovieCard = ({ movie }) => {
           </div>
         </div>
       </div>
-      <CheckLoginModal
-        show={showCheckLoginModal}
-        setShow={setShowCheckLoginModal}
-      />
-      <LoadingModal show={showLoading} />
     </>
   );
 };
