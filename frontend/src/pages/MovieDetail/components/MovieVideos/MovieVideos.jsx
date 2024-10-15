ef00192388b4d63b2e624c62a53f38692397d8c1
kimsh0306 <b666790@gmail.com>
2024-10-15 19:33:18 +0900

    Chore: 코드 수정

--- frontend/src/pages/MovieDetail/components/MovieVideos/MovieVideos.jsx
+++ frontend/src/pages/MovieDetail/components/MovieVideos/MovieVideos.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import YouTube from "react-youtube";
 import { useMovieVideosQuery } from "../../../../hooks/useMovieDetail";
+import YouTube from "react-youtube";
 import { Alert } from "react-bootstrap";
-import "./MovieVideos.style.css";
 import LoadingModal from "../../../../common/LoadingModal/LoadingModal";
+import "./MovieVideos.style.css";
 
 const MovieVideos = ({ id }) => {
   const { data, isLoading, isError, error } = useMovieVideosQuery(id);
@@ -26,6 +26,7 @@ const MovieVideos = ({ id }) => {
   return (
     <div className="video-comp">
       <YouTube
+        className="youtube"
         videoId={data?.results[0]?.key}
         opts={opts}
         iframeClassName="responsive-iframe" // iframe에 클래스 적용
