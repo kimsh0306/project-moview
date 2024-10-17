923973abbe4088c54b82ac0c689114b67e9a9318
kimsh0306 <b666790@gmail.com>
2024-10-17 17:05:16 +0900

    Fix: 불필요한 렌더링 제거

--- frontend/src/common/MovieSlider/MovieSlider.jsx
+++ frontend/src/common/MovieSlider/MovieSlider.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import Carousel from 'react-multi-carousel'
-import MovieCard from '../MovieCard/MovieCard'
+import React from "react";
+import Carousel from "react-multi-carousel";
+import MovieCard from "../MovieCard/MovieCard";
 import "react-multi-carousel/lib/styles.css";
-import "./MovieSlider.style.css"
+import "./MovieSlider.style.css";
 
-const MovieSlider = ({title, movies, responsive, noTitle=false}) => {
+const MovieSlider = ({ title, movies, responsive, noTitle = false }) => {
   return (
     <div className="slide-box">
       {noTitle || <h3>{title}</h3>}
@@ -12,16 +12,16 @@ const MovieSlider = ({title, movies, responsive, noTitle=false}) => {
         draggable={true}
         centerMode={false}
         responsive={responsive}
-        infinite={true}
+        infinite={false} // true일 때, 불필요한 렌더링 문제 발생
         itemClass="movie-slider p-1"
         containerClass="carousel-container"
       >
-        {movies.map((movie, idx)=>(
-          <MovieCard movie={movie} key={idx}/>
+        {movies.map((movie, idx) => (
+          <MovieCard movie={movie} key={`${movie.id}-${idx}`} />
         ))}
       </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default MovieSlider
+export default MovieSlider;
