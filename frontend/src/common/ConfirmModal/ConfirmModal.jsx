e0c6d6f4f4d341aad8314b6833c5fb887989cd02
kimsh0306 <b666790@gmail.com>
2024-10-30 16:43:25 +0900

    Chore: 코드 정리

--- frontend/src/common/ConfirmModal/ConfirmModal.jsx
+++ frontend/src/common/ConfirmModal/ConfirmModal.jsx
@@ -4,6 +4,8 @@ import Modal from "react-bootstrap/Modal";
 import { ImNotification } from "react-icons/im";
 
 const ConfirmModal = ({ show, handleClose, handleConfirm }) => {
+  if (!show) return null;
+  
   return (
     <Modal show={show} onHide={handleClose} centered backdrop="static" keyboard={false}>
       <Modal.Header closeButton>
