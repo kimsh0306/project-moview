f3468082235df14ef1e734b6c0b455cb43acfab2
kimsh0306 <b666790@gmail.com>
2025-09-02 16:14:29 +0900

    Refactor: useInfiniteMovies 를 MoviesViewer 안으로 구조 변경

--- frontend/src/pages/MoviesPage/MoviesPage.jsx
+++ frontend/src/pages/MoviesPage/MoviesPage.jsx
@@ -1,48 +1,10 @@
 import React from "react";
-import { Alert } from "react-bootstrap";
 import { MovieFilterProvider } from "context/filter";
-import { useMovieFilterState } from "hooks/filter";
-import { useInfiniteMovies } from "hooks/movie/useInfiniteMovies";
-import LoadingModal from "common/LoadingModal/LoadingModal";
 import SortSelector from "pages/MoviesPage/components/SortSelector/SortSelector";
 import GenreSelector from "pages/MoviesPage/components/GenreSelector/GenreSelector";
 import MoviesViewer from "pages/MoviesPage/components/MoviesViewer/MoviesViewer";
 import CustomContainer from "common/CustomContainer/CustomContainer";
 import ScrollToTop from "common/ScrollToTop/ScrollToTop";
-
-const FilterBlock = () => {
-  return (
-    <>
-      <SortSelector pageTitle="영화" />
-      <GenreSelector />
-    </>
-  );
-};
-
-const ContentBlock = () => {
-  const { sortOption, genreIds } = useMovieFilterState(); // page 제거
-
-  const { movies, loadMore, hasMore, isLoading, isError, error } =
-    useInfiniteMovies(sortOption, genreIds);
-
-  // 첫 로딩시에만 모달 표시
-  if (isLoading && movies.length === 0) {
-    return <LoadingModal show={true} />;
-  }
-
-  if (isError) {
-    return <Alert variant="danger">{error.message}</Alert>;
-  }
-
-  return (
-    <MoviesViewer
-      movies={movies}
-      loadMore={loadMore}
-      hasMore={hasMore}
-      isLoading={isLoading}
-    />
-  );
-};
 
 const MoviesPage = () => {
   console.log("🔥 MoviesPage 컴포넌트 리렌더링!");
@@ -50,8 +12,9 @@ const MoviesPage = () => {
     <MovieFilterProvider>
       <main className="movie-page">
         <CustomContainer>
-          <FilterBlock />
-          <ContentBlock />
+          <SortSelector pageTitle="영화" />
+          <GenreSelector />
+          <MoviesViewer />
         </CustomContainer>
         <ScrollToTop />
       </main>
--- frontend/src/pages/MoviesPage/components/MoviesViewer/MoviesViewer.jsx
+++ frontend/src/pages/MoviesPage/components/MoviesViewer/MoviesViewer.jsx
@@ -1,27 +1,46 @@
-import React, { memo } from "react";
-import { Row, Col, Spinner } from "react-bootstrap";
-import FlipCard from "common/FlipCard/FlipCard";
+import React from "react";
+import { useMovieFilterState } from "hooks/filter";
+import { useInfiniteMovies } from "hooks/movie/useInfiniteMovies";
 import { useInfiniteScroll } from "hooks/utils/useInfiniteScroll";
+import { Row, Col, Spinner, Alert } from "react-bootstrap";
+import FlipCard from "common/FlipCard/FlipCard";
+import LoadingModal from "common/LoadingModal/LoadingModal";
 
-const MoviesViewer = ({ movies, loadMore, hasMore, isLoading }) => {
-  console.log('🔥 MoviesViewer 컴포넌트 리렌더링!');
-  
+const MoviesViewer = () => {
+  console.log("🔥 MoviesViewer 컴포넌트 리렌더링!");
+
+  const { sortOption, genreIds } = useMovieFilterState();
+  const { movies, loadMore, hasMore, isLoading, isError, error } =
+    useInfiniteMovies(sortOption, genreIds);
   const { lastElementRef, isFetching } = useInfiniteScroll(loadMore, hasMore);
+
+  // 첫 로딩시에만 모달 표시
+  if (isLoading && movies.length === 0) {
+    return <LoadingModal show={true} />;
+  }
+
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
 
   return (
     <section className="movie-viewer">
       <Row className="g-2">
         {movies.map((movie, index) => (
-          <Col 
-            key={`${movie.id}_${index}`} 
-            xl={2} lg={3} md={4} sm={6} xs={6}
+          <Col
+            key={`${movie.id}_${index}`}
+            xl={2}
+            lg={3}
+            md={4}
+            sm={6}
+            xs={6}
             ref={index === movies.length - 1 ? lastElementRef : null} // 마지막 요소에 ref 추가
           >
             <FlipCard movie={movie} />
           </Col>
         ))}
       </Row>
-      
+
       {/* 로딩 스피너 */}
       {(isLoading || isFetching) && (
         <Row className="mt-4">
@@ -32,7 +51,7 @@ const MoviesViewer = ({ movies, loadMore, hasMore, isLoading }) => {
           </Col>
         </Row>
       )}
-      
+
       {/* 더 이상 데이터가 없을 때 */}
       {!hasMore && movies.length > 0 && (
         <Row className="mt-4">
@@ -45,4 +64,4 @@ const MoviesViewer = ({ movies, loadMore, hasMore, isLoading }) => {
   );
 };
 
-export default memo(MoviesViewer);
+export default MoviesViewer;
