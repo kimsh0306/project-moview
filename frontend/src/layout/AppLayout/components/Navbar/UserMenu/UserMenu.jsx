50c2020b5833a07d25c9c03cbaadac9486c5539c
kimsh0306 <b666790@gmail.com>
2025-09-08 23:44:36 +0900

    Refactor: 컴포넌트 분리

--- frontend/src/layout/AppLayout/components/Navbar/UserMenu/UserMenu.jsx
+++ frontend/src/layout/AppLayout/components/Navbar/UserMenu/UserMenu.jsx
@@ -5,22 +5,21 @@ import AccountMenu from "./components/AccountMenu/AccountMenu";
 import LoginButton from "./components/LoginButton/LoginButton";
 import "./UserMenu.style.scss";
 
+const LoginUserBlock = ({ userState }) => {
+  return (
+    <>
+      <SessionTimer expirationTime={userState.exp} />
+      <AccountMenu userId={userState.userId} />
+    </>
+  );
+};
+
 const UserMenu = () => {
   const userState = useSelector((state) => state.auth.user);
-  const { userId, exp: expirationTime } = userState || {};
-
-  const LoginUserBlock = () => {
-    return (
-      <>
-        <SessionTimer expirationTime={expirationTime} />
-        <AccountMenu userId={userId} />
-      </>
-    );
-  };
 
   return (
     <div className="user-menu d-flex align-items-center">
-      {userState ? <LoginUserBlock /> : <LoginButton />}
+      {userState ? <LoginUserBlock userState={userState} /> : <LoginButton />}
     </div>
   );
 };
